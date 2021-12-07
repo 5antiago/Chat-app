bba46489f6f77d6480bb0cf39552aaf85ba6e6d1
Santiago Graziano <santiagograziano534@gmail.com>
2021-12-07 11:46:23 -0300

    added user list, users conected

--- server/main.js
+++ server/main.js
@@ -6,20 +6,27 @@ const path = require("path");
 const http = require("http");
 const server = http.createServer(app);
 
-const dotenv = require("dotenv");
+const env = require("dotenv/config");
 
 const {Server} = require("socket.io");
 
 const io = new Server(server);
+
+var userlist = [];
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname,'..', 'client/index.html'));
 });
 
 io.on('connection', (socket) => {
-    let username;
+    let username;  
+    io.emit('usersconnected', userlist);
+
     socket.on('disconnect', (msg)=>{
-        socket.broadcast.emit('userdisconected', `${username} se desconectó`); 
+        if(username === undefined) return
+        socket.broadcast.emit('userdisconnected', `${username} se desconectó`);
+        userlist.pop(username);
+        socket.broadcast.emit('usersconnected', userlist);
     });
     socket.on('message', (msg)=>{
         socket.broadcast.emit('message', msg);
@@ -29,10 +36,12 @@ io.on('connection', (socket) => {
     });
     socket.on('register', (msg)=>{
         username = msg;
+        userlist.push(username);
+        io.emit('usersconnected', userlist);
         socket.broadcast.emit('register', `${username} se ha unido`)
     });
+    
 });
-
 server.listen(process.env.PORT, () => {
     console.log("Server running");
 });
